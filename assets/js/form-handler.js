c24af1fe457fb7f4688c41886259298f8a8b4fe4
elmaksimka <reval1vity1337@gmail.com>
2023-06-13 21:43:36 +0300

    fixed some issues

--- assets/js/form-handler.js
+++ assets/js/form-handler.js
@@ -1,13 +1,15 @@
 function submitForm(event) {
   event.preventDefault();
-  
-  document.getElementById("success-message").style.display = "none";
 
-  let name = document.getElementById("name").value;
-  let email = document.getElementById("email").value;
-  let message = document.getElementById("message").value;
+  // document.getElementById("success-message").style.display = "none";
+  const test = document.getElementById("success-message");
+  console.log(test);
 
-  let payload = {
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const message = document.getElementById("message").value;
+
+  const payload = {
     name: name,
     email: email,
     message: message,
